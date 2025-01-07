193f4745357add9f6ce1295aaa5c485065b0c205
xonery <kilian.houpeurt@gmail.com>
2025-01-07 22:59:47 +0100

    add train command

---
+++ cli/cli/cmd/train.py
@@ -0,0 +1,30 @@
+# tweet,annotators,gender_annotators,age_annotators,ethnicities_annotators,study_levels_annotators,countries_annotators,labels_task1,labels_task2,ID
+class TrainData:
+
+    ID: int
+    tweet: str
+
+    # Annotation
+    annotators: list[str]
+    age_annotators: list[str]
+    gender_annotators: list[str]
+    countries_annotators: list[str]
+    ethnicities_annotators: list[str]
+    study_levels_annotators: list[str]
+
+    # Labels
+    labels_task1: list[str]
+    labels_task2: list[str]
+
+    def __init__(self, **fields):
+            self.__dict__.update(**fields)
+
+class TrainSet:
+
+    dataset: list[TrainData]
+
+    def __init__(self):
+        self.dataset = []
+
+    def add(self, data: TrainData):
+        self.dataset.append(data)
--- cli/cli/main.py
+++ cli/cli/main.py
@@ -1,4 +1,8 @@
+import csv
+import time
 import typer
+
+from typing_extensions import Annotated
 
 app = typer.Typer(help="This is a CLI tool detect sexism in text.", no_args_is_help=True)
 
@@ -9,3 +13,18 @@ def ping():
 @app.command()
 def hello(name: str):
     typer.echo(f"Hello {name}")
+
+@app.command()
+def train(file: Annotated[typer.FileText, "CSV file used for training"]):
+    from .cmd.train import TrainSet, TrainData
+
+    trainset = TrainSet()
+    
+    reader = csv.DictReader(file)
+
+    from rich.progress import track
+
+    for row in track(reader, description="Loading CSV file"):
+        trainset.add(TrainData(**row))
+
+    typer.echo(f"Loaded {len(trainset.dataset)} rows")
