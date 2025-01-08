da0c28244dbf72a31dca1b3261aefc2400650118
noneofyourbusinessbaby <nadine.hajj-hassa@insa-lyon.fr>
2025-01-08 11:30:02 +0100

    model creation added

--- cli/cli/cmd/train.py
+++ cli/cli/cmd/train.py
@@ -10,6 +10,10 @@ from sklearn.feature_selection import f_classif
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+from tensorflow.python.keras import models
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dropout
+
 
 class TrainingSet:
 
@@ -144,6 +148,47 @@ class TrainSet:
             training_set.add(data.tweet, is_sexist)
 
         return training_set
+
+    def _get_last_layer_units_and_activation(num_classes):
+        """Gets the # units and activation function for the last network layer.
+
+        # Arguments
+            num_classes: int, number of classes.
+
+        # Returns
+            units, activation values.
+        """
+        if num_classes == 2:
+            activation = "sigmoid"
+            units = 1
+        else:
+            activation = "softmax"
+            units = num_classes
+        return units, activation
+
+    def mlp_model(self, layers, units, dropout_rate, input_shape, num_classes):
+        """Creates an instance of a multi-layer perceptron model.
+
+        # Arguments
+            layers: int, number of `Dense` layers in the model.
+            units: int, output dimension of the layers.
+            dropout_rate: float, percentage of input to drop at Dropout layers.
+            input_shape: tuple, shape of input to the model.
+            num_classes: int, number of output classes.
+
+        # Returns
+            An MLP model instance.
+        """
+        op_units, op_activation = self._get_last_layer_units_and_activation(num_classes)
+        model = models.Sequential()
+        model.add(Dropout(rate=dropout_rate, input_shape=input_shape))
+
+        for _ in range(layers - 1):
+            model.add(Dense(units=units, activation="relu"))
+            model.add(Dropout(rate=dropout_rate))
+
+        model.add(Dense(units=op_units, activation=op_activation))
+        return model
 
 
 def run(
