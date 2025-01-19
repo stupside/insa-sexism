74e30f3f304050a2e3aa990d3541eccb3a490175
noneofyourbusinessbaby <nadine.hajj-hassa@insa-lyon.fr>
2025-01-19 22:23:04 +0100

    add device print

--- cli/cli/src/model.py
+++ cli/cli/src/model.py
@@ -15,6 +15,7 @@ if torch.cuda.is_available():
     device = torch.device("cuda")
 else:
     device = torch.device("cpu")
+print(f"Using device: {device}")
 
 # Add logging configuration
 logging.basicConfig(level=logging.INFO)
